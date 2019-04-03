9065e9f52363e561d1265e500366d204a7867e8c
Slava Fedorov <fslava@gmail.com>
2019-04-03 11:21:47 +0300

    Add placeholder to input fields of form

--- src/Views/FormView.php
+++ src/Views/FormView.php
@@ -5,16 +5,17 @@ use fvy\Korus\Utils\HtmlHelpers;
 ?>
 <form autocomplete="off" action="<?= $_SERVER["PHP_SELF"]; ?>" method="post" class="form-inline sandbox-form"
       id="sandbox-container">
+    Фильтр по дате:
     <div class="row form-horizontal">
         <div class="span5 col-md-8" id="sandbox-container">
             <div class="input-daterange input-group" id="datepicker">
-                <input type="text" class="input-sm form-control" name="startDate"
+                <input type="text" class="input-sm form-control" name="startDate" placeholder="C даты"
                        value="<?= HtmlHelpers::sanitizeDate('startDate'); ?>">
-                <span class="input-group-addon">to</span>
-                <input type="text" class="input-sm form-control" name="endDate"
+                <span class="input-group-addon"> - </span>
+                <input type="text" class="input-sm form-control" name="endDate" placeholder="по дату"
                        value="<?= HtmlHelpers::sanitizeDate('endDate'); ?>">
             </div>
-            <button type="submit" class="btn btn-primary btn-sm">Submit</button>
+            <button type="submit" class="btn btn-primary btn-sm">Применить</button>
         </div>
     </div>
 </form>

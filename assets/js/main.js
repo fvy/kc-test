d1a2b3a2ed76f33b567f5883eb93d20833500fd6
Slava Fedorov <fslava@gmail.com>
2019-04-03 17:19:30 +0300

    description for tooltip update

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ $(function () {
 /*            delay: { "show": 300, "hide": 0 },*/
             template: '<div class="tooltip">' +
                 '<div class="tooltip-arrow"></div>' +
-                '<div class="tooltip-head"><h3><span class="glyphicon glyphicon-info-sign"></span>Описание</h3></div>' +
+                '<div class="tooltip-head"><h3><span class="glyphicon glyphicon-info-sign"></span>Описание пользователя</h3></div>' +
                 '<div class="tooltip-inner"></div>' +
                 '</div>'
         }

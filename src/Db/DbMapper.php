057fa5688677b0177aba3beda899653a4de340e4
Slava Fedorov <fslava@gmail.com>
2019-04-03 06:03:37 +0300

    filter the dates && change sql to get correct sum for employees

--- src/Db/DbMapper.php
+++ src/Db/DbMapper.php
@@ -20,26 +20,39 @@ class DbMapper
 
     public static function usersList($startDate = null, $endDate = null)
     {
+        // filter the dates
+        $startDate = self::filterTheDate($startDate);
+        $endDate = self::filterTheDate($endDate);
+
+        // pass without the dates
         if (empty($startDate) && empty($endDate)) {
             return self::usersListWoTime();
         }
+
+        // go with period
+        $dateStr = (!empty($startDate) ? 'AND `Date` >= :startDate' : '') . ' ' .
+            (!empty($endDate) ? 'AND `Date` <= :endDate' : '');
 
         $sth = self::$db->prepare('
             SELECT 
                 Id, Name, Email, userlvl(id) AS path, EmployerId, 
                 TIME_FORMAT(
-                    (select sum(`Time`) from timesheet WHERE id=EployeeId ' .
-            (!empty($startDate) ? 'AND `Date` >= :startDate' : '') . ' ' .
-            (!empty($endDate) ? 'AND `Date` <= :endDate' : '') . '
+                    (select sum(`Time`) from timesheet WHERE id=EployeeId ' . $dateStr . '
                     ), "%H:%i:%s"
                 ) utime, 
+                TIME_FORMAT(
+                    (SELECT sum(`Time`) 
+                    FROM users
+                    LEFT JOIN timesheet ON (Id=EployeeId)
+                    WHERE userlvl(id) LIKE CONCAT(CONVERT(path using utf8),"%") ' . $dateStr . '
+                    ), 
+                    "%H:%i:%s"
+                ) AS totalsum,
                 Info
             FROM 
                 users u
             WHERE 
-                u.Id in (select EployeeId from timesheet WHERE 1=1 ' .
-            (!empty($startDate) ? 'AND `Date` >= :startDate' : '') . ' ' .
-            (!empty($endDate) ? 'AND `Date` <= :endDate' : '') . '
+                u.Id in (select EployeeId from timesheet WHERE 1=1 ' . $dateStr . '
                 )                     
             ORDER BY path
             ');
@@ -62,7 +75,14 @@ class DbMapper
                 Id, Name, Email, userlvl(id) AS path, EmployerId, 
                 TIME_FORMAT(
                     (select sum(`Time`) from timesheet WHERE id=EployeeId), "%H:%i:%s"
-                ) utime, 
+                ) AS utime,
+                TIME_FORMAT(
+                    (SELECT sum(`Time`) 
+                    FROM users
+                    LEFT JOIN timesheet ON (Id=EployeeId)
+                    WHERE userlvl(id) LIKE CONCAT(CONVERT(path using utf8),"%")), 
+                    "%H:%i:%s"
+                ) AS totalsum,
                 Info
             FROM 
                 users u
@@ -72,7 +92,18 @@ class DbMapper
             ');
 
         $sth->execute();
-
+        //$sth->debugDumpParams();
         return $sth->fetchAll();
+    }
+
+    public static function filterTheDate($date, $mask = "@{0-9},4-{0-9/}2-{0-9/}2@")
+    {
+        return filter_var(
+            $date,
+            FILTER_VALIDATE_REGEXP,
+            array(
+                "options" => array("regexp" => $mask)
+            )
+        );
     }
 }
